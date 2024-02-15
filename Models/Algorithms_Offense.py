8bbe6fffe85823cf64d09317baa2b7eeefa9c782
Jerry Kingsbury <jerry.daniel.kingsbury@gmail.com>
2024-02-15 10:54:15 -0600

    Finished Offensive Algorithms

--- Models/Algorithms_Offense.py
+++ Models/Algorithms_Offense.py
@@ -11,58 +11,32 @@ def Calculate_Turnover_Percentage(turnover, field_goals_attempted, free_throws_a
     turnoverpercentage = 100 * turnover / (field_goals_attempted + 0.44 * free_throws_attempted + turnovers)
     return turnoverpercentage
 
-def OffRebPerc():
-    100 * (ORB * (TMP / 5) / (MP * (TORB + ODRB))
-    # ORB=Offensive Rebounds
-    # TMP=Team Minutes Played
-    # MP=Minutes Played
-    # TORB=Team Offensive Rebounds
-    # ODRB=Opponents Defensive Rebounds
+def Calculate_Offensive_Rebound_Percentage(offensive_rebounds, team_minutes_played, minutes_played, team_offensive_rebounds, opponents_defensive_rebounds):
+    offensivereboundpercentage = 100 * (offensive_rebounds * (team_minutes_played / 5)) / (minutes_played * (team_offensive_rebounds + opponents_defensive_rebounds))
+    return offensivereboundpercentage
 
-def eFGPerc():
-    (FG + 0.5 * 3P) / FGA
-    # FG = Field Goals
-    # 3P = Three Pointers
-    # FGA = Field Goal Attempts
+def Calculate_Effective_Field_Goal_Percentage(field_goals, three_point_field_goals, field_goals_attempted):
+        effectivefieldgoalpercentage = (field_goals + 0.5 * three_point_field_goals) / field_goals_attempted
+        return effectivefieldgoalpercentage
 
-def TrueShootingPerc():
-    PT / (2 * (FGA + 0.44 * FTA))
-    # PT = Points
-    # FGA = Field - Goal Attempts
-    # FTA = Free Throw Attempts
+def Calculate_True_Shooting_Percentage(points, field_goals_attempted, free_throws_attempted):
+    trueshootingpercentage = points / (2 * (field_goals_attempted + 0.44 * free_throws_attempted))
+    return trueshootingpercentage
 
-def UsgRate():
-    100 * ((FGA + 0.44 * FTA + TO) * (TMP / 5)) / (MP * (TFGA + 0.44 * TFTA + TTO))
-    # FGA = Field - Goal Attempts
-    # FTA = Free - Throw Attempts
-    # TO = Turnovers
-    # TMP = Team Minutes Played
-    # MP = Minutes Played
-    # TFGA = Team
-    # Field - Goal Attempts,
-    # TFTA = Team Free Throw Attempts
-    # TTO = Team Turnovers
+def Calculate_Usage_Rate(field_goals_attempted, free_throws_attempted, turnovers, team_minutes_played, minutes_played, team_field_goals_attempted, team_free_throws_attempted, team_turnovers):
+    usgrate = 100 * ((field_goals_attempted + 0.44 * free_throws_attempted + turnovers) * (team_minutes_played / 5)) / (minutes_played * (team_field_goals_attempted + 0.44 * team_free_throws_attempted + team_turnovers))
+    return usgrate
 
-def PPP():
-    PT / (FGA + 0.44 * FTA + TO)
-    # PT = Points
-    # FGA = Field - Goal Attempts
-    # FTA = Free - Throw Attempts
-    # TO = Turnovers
+def Calculate_Points_Per_Possession(points, field_goals_attempted, free_throws_attempted, turnovers):
+    pointsperpossession = points / (field_goals_attempted + 0.44 * free_throws_attempted + turnovers)
+    return pointsperpossession
 
-def ORTg():
-    100 * PP / (FGA + 0.44 * FTA + TO)
-    # PP = Points Produced
-    # FGA = Field - Goal Attempts
-    # FTA = Free - Throw Attempts
+def Calculate_Offensive_Rating(points, field_goals_attempted, free_throws_attempted, turnovers, team_possessions):
+    offensiverating = 100 * (points / (field_goals_attempted + 0.44 * free_throws_attempted + turnovers)) / team_possessions
+    return offensiverating
 
-def OFFWinSh():
-    (PP - 0.92 * LPPP * (FGA + 0.44 * FTA + TO)) / (0.32 * LPPG * (TP / LP))
-    # PP = Points Produced
-    # LPPP = League Points Per Possession
-    # FGA = Field - Goal Attempts
-    # FTA = Free - Throw Attempts
-    # TO = Turnovers
-    # LPPG = League Points Per Game
-    # TP = Team Pace
-    # LP = League Pace
+def Calculate_Offensive_Win_Shares(points, league_points_per_possession, field_goals_attempted, free_throws_attempted, turnovers, league_points_per_game, team_pace, league_pace):
+    offensivewinshares = (points - 0.92 * league_points_per_possession * (field_goals_attempted + 0.44 * free_throws_attempted + turnovers)) / (0.32 * league_points_per_game * (team_pace / league_pace))
+    return offensivewinshares
+
+
