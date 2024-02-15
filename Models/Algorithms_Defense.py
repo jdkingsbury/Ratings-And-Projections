b8c0537ec34d6ef77081beb4c0efc05c1176bafb
Jerry Kingsbury <jerry.daniel.kingsbury@gmail.com>
2024-02-15 11:13:32 -0600

    Added Defensive rating algorithms

--- Models/Algorithms_Defense.py
+++ Models/Algorithms_Defense.py
@@ -2,21 +2,23 @@
 import math
 import numpy as np
 
-# TODO: Write all the defensive algorithms
-def StlPerc():
-    100 * (S * (TMP / 5)) / (MP * OP)
-    # S = Steals
-    # TMP = Team Minutes Played
-    # MP = Minutes Played
-    # OP = Opponents Possessions
+def Calculate_Steal_Percentage(steals, team_minutes_played, minutes_played, opponents_possessions):
+    stealpercentage = 100 * (steals * (team_minutes_played / 5)) / (minutes_played * opponents_possessions)
+    return stealpercentage
 
-def BlkPerc():
-    100 * (B * (TMP / 5)) / (MP * (OFGA - O3PA))
-    # B = Blocks
-    # TMP = Team Minutes Played
-    # MP = Minutes Played
-    # OFGA = Opponent Field Goal Attempts
-    # O3PA = Opponent Three Point Attempts
+def Calculate_Block_Percentage(blocks, team_minutes_played, minutes_played, opponents_field_goals_attempted, opponents_three_point_field_goals_attempted):
+    blockpercentage = 100 * (blocks * (team_minutes_played / 5)) / (minutes_played * (opponents_field_goals_attempted - opponents_three_point_field_goals_attempted))
+    return blockpercentage
 
-def DefRebPerc():
-    100 * (DRB * (TMP / 5) / (MP * (TORB + ODRB))
+def Calculate_Defensive_Rebound_Percentage(defensive_rebounds, team_minutes_played, minutes_played, team_offensive_rebounds, opponents_defensive_rebounds):
+    defensivereboundpercentage = 100 * (defensive_rebounds * (team_minutes_played / 5)) / (minutes_played * (team_offensive_rebounds + opponents_defensive_rebounds))
+    return defensivereboundpercentage
+
+# Need to read the book Dean Oliver's book Basketball on Paper to get correct calculation
+def Calculate_Defensive_Rating(opponents_points, team_defensive_possessions):
+    defensiverating = 100 * opponents_points / team_defensive_possessions
+    return defensiverating
+
+def Calculate_Defensive_Win_Shares(minutes_played, team_minutes_played, team_defensive_possession, league_points_per_possession, defensive_rating, league_points_per_game, team_pace, league_pace):
+    defensivewinshares = ((minutes_played/team_minutes_played)*team_defensive_possession)*(((1.08*league_points_per_possession)-(defensive_rating/100))/((0.32*league_points_per_game)*(team_pace/league_pace)))
+    return defensivewinshares
