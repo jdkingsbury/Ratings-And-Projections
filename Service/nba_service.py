97a5ed4abd0f8d171547543e225e3f5f70606cad
Jerry Kingsbury <jerry.daniel.kingsbury@gmail.com>
2024-02-19 14:33:50 -0600

    Able to retrieve player id for a specific player and career stats for a specific player

--- Service/nba_service.py
+++ Service/nba_service.py
@@ -1,5 +1,6 @@
 import json
 import nba_api.stats.endpoints as CommonAllPlayers
+import nba_api.stats.endpoints as playercareerstats
 
 # LeagueID: 00 = NBA, 10 = WNBA, 20 = G-League
 
@@ -13,10 +14,24 @@ def get_all_players(season_year):
     df_common_all_players = common_all_players.get_data_frames()[0]
     return df_common_all_players.to_json(orient='records', lines=True)
 
+# Get player id from player name
+def get_player_id(player_name):
+    common_all_players = CommonAllPlayers.CommonAllPlayers(
+        is_only_current_season=1,
+        league_id='00'
+    )
+    df_common_all_players = common_all_players.get_data_frames()[0]
+    player_id = df_common_all_players[df_common_all_players['DISPLAY_FIRST_LAST'] == player_name]['PERSON_ID']
+    return player_id.to_json(orient='records', lines=True)
 
-print(get_all_players('2020-21'))
+# Get player career stats
+def get_player_career_stats(player_id):
+    career = playercareerstats.PlayerCareerStats(player_id=player_id)
+    return career.get_data_frames()[0].to_json(orient='records', lines=True)
 
-# # Anthony Davis
+print(get_player_career_stats(203076))
+
+ # Anthony Davis
 # career = playercareerstats.PlayerCareerStats(player_id="203076")
 # career.get_data_frames()[0]
 #
