334c167a01f69adcc443d742ed91806fed2e8067
Jerry Kingsbury <jerry.daniel.kingsbury@gmail.com>
2024-03-04 20:50:06 -0600

    Added code to create the players table if it is not created

--- db/main.py
+++ db/main.py
@@ -16,6 +16,30 @@ def main():
     if conn is not None:
         cursor = conn.cursor()
 
+        # SQL statement to create the players table
+        create_table_query = '''
+        CREATE TABLE IF NOT EXISTS players (
+            person_id SERIAL PRIMARY KEY,
+            display_last_comma_first VARCHAR(255),
+            display_first_last VARCHAR(255),
+            rosterstatus INT,
+            from_year VARCHAR(4),
+            to_year VARCHAR(4),
+            playercode VARCHAR(255),
+            player_slug VARCHAR(255),
+            team_id INT,
+            team_city VARCHAR(50),
+            team_name VARCHAR(50),
+            team_abbreviation VARCHAR(10),
+            team_slug VARCHAR(50),
+            team_code VARCHAR(50),
+            games_played_flag CHAR(1),
+            otherleague_experience_ch CHAR(2)
+        );
+        '''
+        cursor.execute(create_table_query)
+
+        # SQL statement to insert data into the players table
         insert_query = '''
         INSERT INTO players (
             person_id, display_last_comma_first, display_first_last, rosterstatus, from_year, 
