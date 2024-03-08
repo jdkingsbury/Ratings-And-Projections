f691ea3e3d5a9b4539424d8955e074fa8580dc39
Jerry Kingsbury <jerry.daniel.kingsbury@gmail.com>
2024-03-08 15:21:01 -0600

    Refactoring how we will insert data into the tables so that I am not repeating any code. will use the same structre as creating a json file

---
+++ db/data_insertion.py
@@ -0,0 +1,72 @@
+def insert_nba_players(conn, players_data):
+    cursor = conn.cursor()
+
+    # NOTE: SQL statement to create the players table
+    create_table_query = '''
+        CREATE TABLE IF NOT EXISTS players (
+            person_id SERIAL PRIMARY KEY,
+            display_last_comma_first VARCHAR(255),
+            display_first_last VARCHAR(255),
+            rosterstatus INT,
+            from_year VARCHAR(4),
+            to_year VARCHAR(4),
+            playercode VARCHAR(255),
+            player_slug VARCHAR(255),
+            team_id INT,
+            team_city VARCHAR(50),
+            team_name VARCHAR(50),
+            team_abbreviation VARCHAR(10),
+            team_slug VARCHAR(50),
+            team_code VARCHAR(50),
+            games_played_flag CHAR(1),
+            otherleague_experience_ch CHAR(2)
+        );
+        '''
+    cursor.execute(create_table_query)
+
+    # NOTE: SQL statement to insert data into the players table
+    insert_query = '''
+        INSERT INTO players (
+            person_id, display_last_comma_first, display_first_last, rosterstatus, from_year, 
+            to_year, playercode, player_slug, team_id, team_city, team_name, 
+            team_abbreviation, team_slug, team_code, games_played_flag, otherleague_experience_ch
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT (person_id) DO NOTHING;
+        '''
+
+    for player in players_data:
+        player_values = (
+            player["PERSON_ID"],
+            player["DISPLAY_LAST_COMMA_FIRST"],
+            player["DISPLAY_FIRST_LAST"],
+            player["ROSTERSTATUS"],
+            player["FROM_YEAR"],
+            player["TO_YEAR"],
+            player["PLAYERCODE"],
+            player["PLAYER_SLUG"],
+            player["TEAM_ID"],
+            player["TEAM_CITY"],
+            player["TEAM_NAME"],
+            player["TEAM_ABBREVIATION"],
+            player["TEAM_SLUG"],
+            player["TEAM_CODE"],
+            player["GAMES_PLAYED_FLAG"],
+            player["OTHERLEAGUE_EXPERIENCE_CH"]
+        )
+        cursor.execute(insert_query, player_values)
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+
+def insert_player_career_stats(conn, data):
+    pass
+
+
+# NOTE: Function mapping for data insertion
+# Add new functions here as needed
+function_mapping = {
+    "get_player_career_stats": insert_player_career_stats,
+    "nba_players": insert_nba_players,
+}
---
+++ db/main.py
@@ -0,0 +1,36 @@
+import sys
+from pathlib import Path
+from .db_operations import connect_to_database
+from .json_reader import read_json_file
+from .data_insertion import function_mapping
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: python main.py <json_file_path> <table_name>")
+        return
+    
+    function_identifier, arg = sys.argv[1], sys.argv[2]
+    current_script_path = Path(__file__).parent
+    json_file_name = f"{function_identifier}_{arg}.json"
+    json_file_path = current_script_path / f'../data/{json_file_name}'
+    absolute_json_file_path = json_file_path.resolve()
+
+    data = read_json_file(absolute_json_file_path)
+
+    if data is None:
+        print(f"Error: File {json_file_path} not found")
+        return
+
+    conn = connect_to_database()
+
+    if conn is not None and function_identifier in function_mapping:
+        insertion_function = function_mapping[function_identifier]
+        insertion_function(conn, data)
+        conn.close()
+    else:
+        print(f"Function identifier '{function_identifier}' not recognized.")
+
+
+if __name__ == '__main__':
+    main()
