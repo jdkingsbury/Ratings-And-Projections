0f692315fc7949083e76d34221f56c51af26c19c
Jerry Kingsbury <jerry.daniel.kingsbury@gmail.com>
2024-03-08 22:14:52 -0600

    Edited create_json to handle when no arguments are used just in case we have functions that do not need any

--- services/create_json.py
+++ services/create_json.py
@@ -24,17 +24,24 @@ def main():
         print("Usage: python create_json.py <function> [arguments]")
         sys.exit(1)
 
-    function_name, arg = sys.argv[1], sys.argv[2]
+    function_name = sys.argv[1]
+    args = sys.argv[2:]
 
     if function_name not in function_mapping:
         print(f"Invalid function name. Valid options are: {list(function_mapping.keys())}")
         sys.exit(1)
 
-    data = function_mapping[function_name](arg)
+    if args:
+        data = function_mapping[function_name](*args)
+        file_name = f"{function_name}_{'_'.join(args)}.json"
+    else:
+        data = function_mapping[function_name]()
+        file_name = f"{function_name}.json"
 
     if data is not None:
-        create_json_file(data, f"{function_name}_{arg}.json")
-        print(f"Data written to {function_name}_{arg}.json")
+        create_json_file(data, file_name)
+        print(f"Data written to {file_name}")
+
 
 if __name__ == '__main__':
     main()
