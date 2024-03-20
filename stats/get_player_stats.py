2fc6a37521df0ff565ee888b1dd5f5374758e69f
Jerry Kingsbury <jerry.daniel.kingsbury@gmail.com>
2024-03-19 23:31:15 -0500

    Fixed some errors with the function names were wrong from retrieving data. Also made some changes to the calculate_player_grade function

--- stats/get_player_stats.py
+++ stats/get_player_stats.py
@@ -8,8 +8,8 @@ def get_player_stats(player_id, season_id):
         "spg": retrieve_and_calculate_data('get_total_steals_and_games_played', 'spg', player_id, season_id),
         "bpg": retrieve_and_calculate_data('get_total_blocks_and_games_played', 'bpg', player_id, season_id),
         "tov": retrieve_and_calculate_data('get_total_turnovers_and_games_played', 'tov', player_id, season_id),
-        "fg_percent": retrieve_and_calculate_data('get_total_field_goals_and_attempts', 'fg_percent', player_id, season_id),
-        "three_point_percent": retrieve_and_calculate_data('get_total_three_pointers_and_attempts', 'three_point_percent', player_id, season_id),
-        "ft_percent": retrieve_and_calculate_data('get_total_free_throws_and_attempts', 'ft_percent', player_id, season_id),
+        "fg_percent": retrieve_and_calculate_data('get_total_field_goals_made_and_attempted', 'fg_percent', player_id, season_id),
+        "three_point_percent": retrieve_and_calculate_data('get_total_three_pointers_made_and_attempted', 'three_point_percent', player_id, season_id),
+        "ft_percent": retrieve_and_calculate_data('get_total_free_throws_made_and_attempted', 'ft_percent', player_id, season_id),
     }
     return stats
