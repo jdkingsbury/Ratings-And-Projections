d4c24c2c35e7d8bcb1fd3405432d7d09ce4460a1
Jerry Kingsbury <jerry.daniel.kingsbury@gmail.com>
2024-03-26 12:33:56 -0500

    Made two error checkers

--- stats/helper_functions.py
+++ stats/helper_functions.py
@@ -1,5 +1,6 @@
 # Description: Helper functions for stats module
 
+
 # NOTE: Helper function to calculate per game stats
 def calculate_per_game_stat(total_stat, games_played, precision=1):
     return round(total_stat / games_played, precision) if games_played else 0
@@ -10,13 +11,15 @@ def calculate_percentage_stat(made, attempted, precision=1):
     return round((made / attempted) * 100, precision) if attempted else 0.0
 
 
-def validate_stat_data(data, stat_name):
-    if isinstance(data, (tuple, list)) and len(data) == 2 and all(isinstance(x, (int, float)) for x in data):
-        return data
-
-    if isinstance(data, (tuple, list)) and len(data) == 2 and all(isinstance(x, (int, float)) for x in data):
+def validate_average_stat_data(data, stat_name):
+    if (isinstance(data, (tuple, list)) and len(data) == 2 and all(isinstance(x, (int, float)) for x in data)):
         return data
 
     raise ValueError(f"{stat_name} data is invalid or does not contain expected elements")
 
 
+def validate_percent_stat_data(data, stat_name):
+    if isinstance(data, (int, float)):
+        return data
+
+    raise ValueError(f"{stat_name} data is invalid or does not contain expected elements")
--- stats/stats_calculations.py
+++ stats/stats_calculations.py
@@ -1,12 +1,12 @@
 # Description: This file contains functions that are used to calculate stats for players.
 
-from stats.helper_functions import (calculate_per_game_stat, calculate_percentage_stat, validate_stat_data,)
+from stats.helper_functions import (calculate_per_game_stat, validate_average_stat_data, validate_percent_stat_data)
 
 
 # NOTE: Function to calculate points per game
 def calculate_ppg(data):
     if len(data) == 2:
-        total_points, games_played = validate_stat_data(data, "PPG")
+        total_points, games_played = validate_average_stat_data(data, "PPG")
         return calculate_per_game_stat(total_points, games_played)
     return 0
 
@@ -14,47 +14,50 @@ def calculate_ppg(data):
 # NOTE: Function to calculate rebounds per game
 def calculate_rpg(data):
     if len(data) == 2:
-        total_rebounds, games_played = validate_stat_data(data, "RPG")
+        total_rebounds, games_played = validate_average_stat_data(data, "RPG")
         return calculate_per_game_stat(total_rebounds, games_played)
 
 
 # NOTE: Function to calculate assists per game
 def calculate_apg(data):
     if len(data) == 2:
-        total_assists, games_played = validate_stat_data(data, "APG")
+        total_assists, games_played = validate_average_stat_data(data, "APG")
         return calculate_per_game_stat(total_assists, games_played)
 
 
 # NOTE: Function to calculate steals per game
 def calculate_spg(data):
     if len(data) == 2:
-        total_steals, games_played = validate_stat_data(data, "SPG")
+        total_steals, games_played = validate_average_stat_data(data, "SPG")
         return calculate_per_game_stat(total_steals, games_played)
 
 
 # NOTE: Function to calculate blocks per game
 def calculate_bpg(data):
     if len(data) == 2:
-        total_blocks, games_played = validate_stat_data(data, "BPG")
+        total_blocks, games_played = validate_average_stat_data(data, "BPG")
         return calculate_per_game_stat(total_blocks, games_played)
 
 
 # NOTE: Function to calculate turnovers per game
 def calculate_tov(data):
     if len(data) == 2:
-        total_turnovers, games_played = validate_stat_data(data, "TOV")
+        total_turnovers, games_played = validate_average_stat_data(data, "TOV")
         return calculate_per_game_stat(total_turnovers, games_played)
 
+
 def calculate_fg_percent(data):
-    fg_pct = validate_stat_data(data, "FG%")
+    fg_pct = validate_percent_stat_data(data, "FG%")
     return fg_pct
 
+
 def calculate_three_point_percent(data):
-    three_point_pct = validate_stat_data(data, "3P%")
+    three_point_pct = validate_percent_stat_data(data, "3P%")
     return three_point_pct
 
+
 def calculate_ft_percent(data):
-    ft_percent = validate_stat_data(data, "FT%")
+    ft_percent = validate_percent_stat_data(data, "FT%")
     return ft_percent
 
 
