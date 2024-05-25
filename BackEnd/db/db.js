9ae8651ec2b52db55ba66d720076ea7ca748725b
Jerry Kingsbury <jerry.daniel.kingsbury@gmail.com>
2024-05-25 03:33:00 -0500

    Update db.js

--- BackEnd/db/db.js
+++ BackEnd/db/db.js
@@ -1,11 +1,14 @@
 const { Client } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'nba',
-  password: 'postgres',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 client.connect()
