f7736ca65fc8fb455b813c85a19d6e1b52042271
Jerry Kingsbury <jerry.daniel.kingsbury@gmail.com>
2024-06-15 22:52:36 -0500

    Removed setup.js

--- setup.js
+++
@@ -1,8 +0,0 @@
-import { execSync } from "child_process";
-import path from "path";
-import { fileURLToPath } from "url";
-import fs from "fs/promises";
-import pool from "./app/db/db.js";
-import { readJsonFile } from "./app/utils/readJsonFile.js";
-
-
