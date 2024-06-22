a345d7d1646628beb8b0a581c7d00e7742ff00c9
Jerry Kingsbury <jerry.daniel.kingsbury@gmail.com>
2024-06-22 13:02:17 -0500

    Added a route for getting players previous games. Also edited some of the variable names

--- backend/api/routers/nba.py
+++ backend/api/routers/nba.py
@@ -17,12 +17,23 @@ router = APIRouter(prefix="/nba", tags=["nba"])
 
 # NOTE: Possible endpoints
 # Draft History
+# Last 5 games
+# Player News
 
 # TODO:
 # [x] Work on implementing fast api into this project so that we can create api endpoints to use
 # in the application but also allow others to access the data if we make it available to the public
 # [] Clean up file and remove functions we do not need
 # [] Refactor functions so that they are cleaner
+
+# PLAYERS
+
+
+# NOTE: Will get the player_id from players name.
+def get_player_id(player_name):
+    player = players.find_players_by_full_name(player_name)[0]
+    player_id = player["id"]
+    return player_id
 
 
 # NOTE: Get player stats for a specific season
@@ -54,6 +65,15 @@ def get_player_stats(season_year, output_format="json"):
         raise ValueError("Unsupported format. Please choose 'json' or 'csv'.")
 
 
+# NOTE: Static function that gets basic info for all players
+# Might change how I retrieve all players
+@router.get("/players")
+def all_players():
+    nba_players = players.get_players()
+    active_players = [player for player in nba_players if player["is_active"] == True]
+    return JSONResponse(content=active_players)
+
+
 # This function will allow us to get the info of a specific player
 @router.get("/players/{person_id}")
 def get_player_info(person_id, output_format="json"):
@@ -69,19 +89,10 @@ def get_player_info(person_id, output_format="json"):
         player_info_csv = player_info_df.to_csv(index=False)
         return Response(content=player_info_csv, media_type="text/csv")
     elif output_format == "json":
-        player_info_json = player_info_df.to_dict(orient="records")
-        return JSONResponse(content=player_info_json)
+        player_info_dict = player_info_df.to_dict(orient="records")
+        return JSONResponse(content=player_info_dict)
     else:
         raise ValueError("Unsupported format. Please choose 'json' or 'csv'.")
-
-
-# NOTE: Static function that gets basic info for all players
-# Might change how I retrieve all players
-@router.get("/players")
-def all_players():
-    nba_players = players.get_players()
-    active_players = [player for player in nba_players if player["is_active"] == True]
-    return JSONResponse(content=active_players)
 
 
 # NOTE: Get player career stats
@@ -99,25 +110,26 @@ def get_player_career_stats(person_id, output_format="json"):
         player_career_stats_csv = player_career_stats_df.to_csv(index=False)
         return Response(content=player_career_stats_csv, media_type="text/csv")
     elif output_format == "json":
-        player_career_stats_json = player_career_stats_df.to_dict(orient="records")
-        return JSONResponse(content=player_career_stats_json)
+        player_career_stats_dict = player_career_stats_df.to_dict(orient="records")
+        return JSONResponse(content=player_career_stats_dict)
     else:
         raise ValueError("Unsupported format. Please choose 'json' or 'csv'.")
 
 
-# NOTE: Get player game log
-def get_player_game_log(person_id, season_year, output_format="json"):
-    game_log = playergamelog.PlayerGameLog(
-        player_id=person_id, season=season_year, season_type_all_star="Regular Season"
-    )
-    player_game_log = game_log.get_data_frames()[0]
-
-    if "VIDEO_AVAILABLE" in player_game_log.columns:
-        player_game_log = player_game_log.drop(columns=["VIDEO_AVAILABLE"])
+# NOTE: Get players game for the season
+@router.get("players/{person_id}/{season_year}/{games}")
+def get_player_game_log(person_id, season_year, games, output_format="json"):
+    game_log = playergamelog.PlayerGameLog(player_id=person_id, season=season_year)
+    player_game_log_df = game_log.get_data_frames()[0]
+    
+    if games!=0:
+        player_game_log_df.head(5)
 
     if output_format == "csv":
-        return player_game_log.to_csv(index=False)
+        player_game_log_csv = player_game_log_df.to_csv(index=False)
+        return Response(content=player_game_log_csv, media_type="text/csv")
     elif output_format == "json":
-        return player_game_log.to_json(orient="records", indent=4)
+        player_game_log_dict = player_game_log_df.to_dict(orient="records")
+        return JSONResponse(content=player_game_log_dict)
     else:
         raise ValueError("Unsupported format. Please choose 'json' or 'csv'.")
