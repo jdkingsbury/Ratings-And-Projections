1e0eb724dcb116f35dd44d38b62aaabb70dd1bc7
Jerry Kingsbury <jerrykingsbury@Jerrys-MacBook-Pro.local>
2024-08-10 23:04:59 -0500

    Players Page
    Updated the code in the players page to display the correct types for
    typescript and display an error if there is an issue with fetching
    players. Also updated the columns to display the players number,
    position, school, country and players picture next to their full name.
    The layout or table may change.

--- frontend/src/app/nba/players/columns.tsx
+++ frontend/src/app/nba/players/columns.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+import Image from "next/image";
+import { ColumnDef } from "@tanstack/react-table";
+import { Player } from "./types";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ArrowUpDown } from "lucide-react";
-import { ColumnDef } from "@tanstack/react-table";
-import { Player } from "./types";
-import Image from "next/image";
+
+// Starting Layout for the players page. This page will probably change when I find the layout I want.
 
 export const columns: ColumnDef<Player>[] = [
-  // Player Column
+  // Displays the Players Name
   {
+    // Code for sorting players in the table when the user clicks the up and down arrow buttons
     header: ({ column }) => {
       return (
         <div>
@@ -25,33 +28,56 @@ export const columns: ColumnDef<Player>[] = [
         </div>
       );
     },
-    accessorKey: "full_name",
+
+    accessorKey: "first_last",
+
+    // Creates the links to navigate to the players profile page
     cell: ({ row }) => {
       const players = row.original;
 
       return (
-        <div>
-          {/* <Image */}
-          {/*   src={players.image_url} */}
-          {/*   alt={players.first_last} */}
-          {/*   quality={100} */}
-          {/*   width={40} */}
-          {/*   height={40} */}
-          {/*   className="rounded-md object-cover mr-6" */}
-          {/* /> */}
-
-          <Link href={`/nba/players/${players.player_id}`}>
-            {players.first_last}
-          </Link>
+        <div style={{ display: "flex" }}>
+          <div>
+            <Image
+              src={players.image_url}
+              alt={players.first_last}
+              quality={100}
+              width={40}
+              height={40}
+              className="rounded-md object-cover mr-6"
+            />
+          </div>
+          <div>
+            <Link href={`/nba/players/${players.player_id}`}>
+              {players.first_last}
+            </Link>
+          </div>
         </div>
       );
     },
   },
-  // Team
-  // Number
-  // Position
-  // Height
-  // Weight
-  // School
-  // Country
+  {
+    // TODO: Fetch Team Data For the Player
+    header: "Team",
+  },
+  // Displays Players Jersey Number
+  {
+    header: "Number",
+    accessorKey: "jersey",
+  },
+  // Displays Players Position
+  {
+    header: "Position",
+    accessorKey: "position",
+  },
+  // Displays School Player Last Attended
+  {
+    header: "School",
+    accessorKey: "school",
+  },
+  // Displays the Country the Player is from
+  {
+    header: "Country",
+    accessorKey: "country",
+  },
 ];
